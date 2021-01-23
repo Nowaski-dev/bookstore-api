7ce94b944403a5b5bdea3ee66994c9b98bd6f1a4
Nowaski-dev <cnsysteminfbr@gmail.com>
2021-01-23 00:40:41 -0200

    Instancia automatica da base de dados

--- src/main/java/com/cnsystem/bookstore/BookstoreApplication.java
+++ src/main/java/com/cnsystem/bookstore/BookstoreApplication.java
@@ -1,13 +1,43 @@
 package com.cnsystem.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cnsystem.bookstore.domain.Categoria;
+import com.cnsystem.bookstore.domain.Livro;
+import com.cnsystem.bookstore.repositories.CategoriaRepository;
+import com.cnsystem.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica", "Livro de TI");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "lorem Ipsum", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+		
+		
 	}
 
 }
