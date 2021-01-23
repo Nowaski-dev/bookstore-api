56e00b931a995382872493b2bd188ad596cbf599
Nowaski-dev <cnsysteminfbr@gmail.com>
2021-01-23 21:15:32 -0200

    Perfil de dev implementado

---
+++ src/main/java/com/cnsystem/bookstore/config/DevConfig.java
@@ -0,0 +1,27 @@
+package com.cnsystem.bookstore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.cnsystem.bookstore.service.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired
+	private DBService dbService;
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	@Bean
+	public Boolean instaciaBaseDeDados() {
+		if(strategy.equals("create")) {
+			this.dbService.instaciaBaseDeDados();
+		}
+		return false;
+	}
+
+}
