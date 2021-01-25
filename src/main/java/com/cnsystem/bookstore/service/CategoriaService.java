647e452da7db74a64275d6a8f24849ac858cb556
Nowaski-dev <cnsysteminfbr@gmail.com>
2021-01-24 23:31:10 -0200

    UPDATE para Categoria

--- src/main/java/com/cnsystem/bookstore/service/CategoriaService.java
+++ src/main/java/com/cnsystem/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cnsystem.bookstore.domain.Categoria;
+import com.cnsystem.bookstore.dtos.CategoriaDTO;
 import com.cnsystem.bookstore.repositories.CategoriaRepository;
 import com.cnsystem.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -26,6 +27,13 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 
